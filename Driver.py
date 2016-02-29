a088d70c2f89e371424f05233550ce19503739c7
Dan Decker <danieldecker88@gmail.com>
2016-02-29 16:42:04 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,9 +1,12 @@
 from Character import Character
 import Enemy 
+import Encounter
 
 
 
-#player = Character()
-#player.print_char_sheet()
+player = Character()
+player.print_char_sheet()
 
-Enemy.create_mage()
+#Enemy.create_mage()
+
+Encounter.encounter_mage()
